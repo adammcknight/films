0e2e9f29eda294dd64a0a4eb978738cb71de7d6a
unknown <C12542573@BUS-1025-34.ict.ad.dit.ie>
2016-01-26 15:04:24 +0000

    Validations added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
 	belongs_to :genre, dependent: :destroy
+	
+	validates :title, :released_on, presence: true
+	
+	#validates_inclusion_of :rating, in:("PG", "12", "18")
 end
